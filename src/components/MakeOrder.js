f5606dc8e09cc75e880de0d2255927afbe7f074d
ksso730 <ksso7300@gmail.com>
2020-06-05 17:04:41 +0900

    css 변경, xml parsing 테스트중

--- src/components/MakeOrder.js
+++ src/components/MakeOrder.js
@@ -27,37 +27,39 @@ const MakeOrder= () => {
    } = Parser({orders});
   
     return (
-        <form onSubmit={handleSubmit} className='order-form'>
-          <h1>Make Your Order</h1>
-          <div>
-            <label>고객명</label>
-            <input type='text' name='custNm' onChange={handleChange} value={orders.custNm}/>
+      <div id="wrapper">
+        <div className="row">
+            <form onSubmit={handleSubmit} className='order-form'>
+                <h1>Make Your Order</h1>
+                <div>
+                  <label>고객명</label>
+                  <input type='text' name='custNm' onChange={handleChange} value={orders.custNm}/>
+                </div>
+                <div>
+                  <label>상품유형</label>
+                  <input type='text' name='prodNm' onChange={handleChange} value={orders.prodNm}/>
+                </div>
+                <div>
+                  <label>오더타입</label>
+                  <select className='order-type' defaultValue={initialValues.orderType} name='orderType'>
+                    <option selected value={orders.orderType} onChange={handleChange}>4201</option>
+                    <option selected value={orders.orderType} onChange={handleChange}>4202</option>
+                    <option selected value={orders.orderType} onChange={handleChange}>4204</option>
+                    <option selected value={orders.orderType} onChange={handleChange}>4203</option>
+                  </select>
+                </div>
+                <br/>
+                <div className="buttons">
+                  <button className='submit-btn btn' type='submit'>SUBMIT</button>
+                  <button className='parse-btn btn' type='button' onClick={onParsing}>CREATE</button>
+                </div>
+             </form>
+            <textarea className="xml-area" value={mkxml}>
+              </textarea>
           </div>
-          <div>
-            <label>오더타입</label>
-            <select className='order-type' defaultValue={initialValues.orderType} name='orderType'>
-              <option selected value={orders.orderType} onChange={handleChange}>4201</option>
-              <option selected value={orders.orderType} onChange={handleChange}>4202</option>
-              <option selected value={orders.orderType} onChange={handleChange}>4204</option>
-              <option selected value={orders.orderType} onChange={handleChange}>4203</option>
-            </select>
-          </div>
-          <div>
-            <label>상품유형</label>
-            <input type='text' name='prodNm' onChange={handleChange} value={orders.prodNm}/>
-          </div>
-          <br/>
-          <button className='submit-btn' type='submit'>SUBMIT</button>
-          <button className='parse-btn' type='button' onClick={onParsing}>SUBMIT</button>
-          <div>
-            {mkxml}
-          </div>
-        </form>
+        </div>
     );
     
-    return {
-      orders
-    }
 }
 
 export default MakeOrder;
--- src/components/Parser.js
+++ src/components/Parser.js
@@ -13,11 +13,37 @@ const Parser= ({orders})=>{
 
         console.log('Parser End -----------------------');
         //objects: {custNm: "", orderType: "4201", prodNm: ""}
-
         let builder = new xml2js.Builder();
-        let xml = builder.buildObject(objects);
+        let xml = `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/">
+        <soap:Header xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+            <ns2:BITHeader xmlns:ns3="http://www.kt.com/bit/oss/GPNA_LVSP_OSB0001" xmlns:ns2="http://www.kt.com/bit/standard/header/v1">
+                <ns2:InterfaceID>GPNA_LVSP_OSB0001</ns2:InterfaceID>
+                <ns2:SourceSys>GPNA</ns2:SourceSys>
+                <ns2:TargetSys>LVSP</ns2:TargetSys>
+                <ns2:GUID>GPNA_2020052617481125_LVSPWS0001</ns2:GUID>
+                <ns2:SeqNumber>1</ns2:SeqNumber>
+            </ns2:BITHeader>
+        </soap:Header>
+        <soap:Body xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+            <gpna:insertBiznaruOrderInfo xmlns:gpna="http://www.kt.com/bit/oss/GPNA_LVSP_OSB0001">
+                <biznaruOrderInfoRequest>`
+        xml += builder.buildObject(objects);
+        xml.replace('orders', 'insertBiznaruOrderInfo');
+        xml+=`\n</biznaruOrderInfoRequest>
+        </gpna:insertBiznaruOrderInfo>
+    </soap:Body>
+</soapenv:Envelope>`
 
         setMkxml(xml);
+        // const xm = '<soap:Header xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"><ns2:InterfaceID>GPNA_LVSP_OSB0001</ns2:InterfaceID></soap:Header>';
+        // xml2js.parseString(xm, (err, result)=>{
+        //     console.log(result);
+        // });
+
+        const js = '{soap:Header: $:{xmlns:soap: "http://schemas.xmlsoap.org/soap/envelope/"}, ns2:InterfaceID: ["GPNA_LVSP_OSB0001"]}';
+        let xm2 = builder.buildObject(js);
+        console.log(xm2)
+
     }
 
     return{
